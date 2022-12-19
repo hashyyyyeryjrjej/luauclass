7e282945c86e332fad02eacff1bc34ad38d725fe
Ozzy <53915096+hashyyyyeryjrjej@users.noreply.github.com>
2022-12-19 16:18:54 -0300

    Update Class.lua

--- Class.lua
+++ Class.lua
@@ -4,16 +4,16 @@ local Class = {
 Class.__index = Class
 
 -- Works like a pseudo-removal by locking the table from editing
-function Class:Destroy()
+function Class:destroy()
 	table.freeze(self)
 end
 
 -- Compare class to another with strings
-function Class:Is(otherClass)
+function Class:is(otherClass)
 	if typeof(otherClass) == 'table' then
 		otherClass = otherClass.ClassName
 	end
-	
+
 	return tostring(self) == otherClass
 end
 
@@ -28,8 +28,14 @@ function Class.new(self)
 	return setmetatable({}, self)
 end
 
+-- Makes a new object from the super
+-- Good for making .new functions on inherited objects
+function Class:newSuperClassObject(...)
+	return self.__super.new(...)
+end
+
 -- Extension method to inherit fields & functions
-function Class:Extend()
+function Class:extend()
 	if not self then
 		warn('No valid class to extend from; Did you forget to use ":"?')
 		return
